60271878b41f4364839ab43a31f326dae240d649
Adam Pinsky <adam.pinsky4@gmail.com>
2016-08-11 01:17:48 -0500

    Added commenting for documentation

--- Challenges/Roman Numeral Converter.js
+++ Challenges/Roman Numeral Converter.js
@@ -1,32 +1,40 @@
-document.ready(function () {
+document.ready(function() {
     'use strict';
+    /**
+     * Returns the Roman Numeral representation of num
+     * @param {Number} num - any positive whole Number
+     * @returns {String} A String of Roman Numerals equal to num
+     */
     function convertToRoman(num) {
         var answer = '';
         var numeralKey = {
-            'M' : 1000,
-            'CM' : 900,
-            'D' : 500,
-            'CD' : 400,
-            'C' : 100,
-            'XC' : 90,
-            'L' : 50,
-            'XL' : 40,
-            'X' : 10,
-            'IX' : 9,
-            'V' : 5,
-            'IV' : 4,
-            '1' : 1
+            'M': 1000,
+            'CM': 900,
+            'D': 500,
+            'CD': 400,
+            'C': 100,
+            'XC': 90,
+            'L': 50,
+            'XL': 40,
+            'X': 10,
+            'IX': 9,
+            'V': 5,
+            'IV': 4,
+            '1': 1
         };
-        
+        /**
+         * add Roman Numerals to answer one at a time
+         * wile subtracting its value from num
+         */
         for (var numeral in numeralKey) {
-            while (num >== numeralKey[numeral]) {
+            while (num >= numeralKey[numeral]) {
                 answer += numeral;
                 num -= numeralKey[numeral];
             }
         }
-        
-        $('#numerals').text(answer);
-        
+
+        document.getElementById('numerals').innerHTML = answer;
+
         return answer;
     }
 
